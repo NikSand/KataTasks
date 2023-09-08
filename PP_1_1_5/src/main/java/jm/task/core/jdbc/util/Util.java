adf28c7f3bf8d9350d94bea74211d38006554e31
Никита <71456672+NikSand@users.noreply.github.com>
2023-09-08 17:22:50 +0300

    Add files via upload

---
+++ PP_1_1_5/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,56 @@
+package jm.task.core.jdbc.util;
+
+import java.util.Properties;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static SessionFactory sessionFactory;
+    private static final String USER = "root";
+    private static final String PASS = "231618";
+    private static final String URL = "jdbc:mysql://localhost:3306/katatask.1.1.4?useSSL=false";
+    private static final String DB_Driver = "com.mysql.cj.jdbc.Driver";
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+
+                settings.put(Environment.DRIVER, DB_Driver);
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USER);
+                settings.put(Environment.PASS, PASS);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                 settings.put(Environment.HBM2DDL_AUTO, "");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
+
+
